b7aa260a53ce1abde736ae060690b2382923db42
andrea <andrea.peruffo1982@gmail.com>
2018-10-17 17:56:37 +0100

    fixed thread pool

--- run-clean.sh
+++ run-clean.sh
@@ -13,26 +13,33 @@ function run {
   time sbt -sbt-jar $1 -ivy ./ivy2 -sbt-dir ./sbt exit > /dev/null 2> /dev/null
 }
 
-for i in {1..10}
+for i in {1..1}
 do
+  # prepare
+  # echo "parallel"
+  # run sbt-launch-1.0.4-SNAPSHOT-parallel.jar
+
+  # echo
+  # echo
+
+  # prepare
+  # echo "parallel-parallel"
+  # run sbt-launch-1.0.4-SNAPSHOT-parallel-parallel.jar
+
+  # echo
+  # echo
+
   prepare
-  echo "parallel"
-  run sbt-launch-1.0.4-SNAPSHOT-parallel.jar
+  echo "fixed"
+  run sbt-launch-1.0.4-SNAPSHOT-fixed.jar
 
   echo
   echo
 
-  prepare
-  echo "parallel-parallel"
-  run sbt-launch-1.0.4-SNAPSHOT-parallel-parallel.jar
+  # prepare
+  # echo "default"
+  # run sbt-launch-1.0.4-SNAPSHOT.jar
 
-  echo
-  echo
-
-  prepare
-  echo "default"
-  run sbt-launch-1.0.4-SNAPSHOT.jar
-
-  echo
-  echo
+  # echo
+  # echo
 done
